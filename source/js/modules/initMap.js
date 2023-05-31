7de5037847c7ac2efc398f65338a49cab1e48e3e
Anastas <77452311+achernova@users.noreply.github.com>
2023-05-31 19:11:23 +0300

    метка на карте

--- source/js/modules/initMap.js
+++ source/js/modules/initMap.js
@@ -6,43 +6,36 @@ const initMap = () => {
   main();
 
   async function main() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда
-    // загрузятся все компоненты API.
     await ymaps3.ready;
-    // Создание карты.
+
     const map = new ymaps3.YMap(mapYandex, {
       location: {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «долгота, широта».
         center: [firstCoordinates, secondCoordinates],
-
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
         zoom: 11
       }
-      },
+    },
       [
-        // Добавляем слой для отображения схематической карты.
         new ymaps3.YMapDefaultSchemeLayer()
       ]
-    )
-  }
+    );
 
-  const markerElement = document.createElement('div')
-  markerElement.className = 'map-one__marker';
-  // markerElement.innerText = "I'm marker!";
+    const markerElement = document.createElement('div');
+    markerElement.className = 'marker';
 
-  const marker = new YMapMarker(
-    {
-      source: 'markerSource',
-      coordinates: [firstCoordinates, secondCoordinates],
-      draggable: true,
-      mapFollowsOnDrag: true
-    },
-    markerElement
-  );
+    YMapMarker = window.YMapMarker
 
-  mapYandex.addChild(marker);
+    const marker = new YMapMarker(
+      {
+        source: 'markerSource',
+        coordinates: [10.260566, 43.877557],
+        draggable: true,
+        mapFollowsOnDrag: true
+      },
+      markerElement
+    );
+
+    map.addChild(marker);
+  };
 };
 
 export {initMap};
