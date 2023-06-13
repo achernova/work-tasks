0ecc7e6457b1b789b3bec76281b952de73ae1a3a
Anastas <77452311+achernova@users.noreply.github.com>
2023-06-13 21:57:23 +0300

    скролл, параллакс

--- source/js/modules/animation-blocks.js
+++ source/js/modules/animation-blocks.js
@@ -3,7 +3,6 @@ import {ScrollTrigger} from '../vendor/scroll-trigger.min';
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 const animateBlocks = () => {
   gsap.to('[data-element="fade"]', {
     scrollTrigger: {
@@ -12,18 +11,58 @@ const animateBlocks = () => {
     },
     opacity: 1,
     duration: 1,
+    stagger: 0.5,
   });
 
   gsap.to('[data-element="fade-in"]', {
     scrollTrigger: {
       trigger: '[data-element="fade-in"]',
-      markers: true,
-      start: 'top 60%',
+      start: 'top 50%',
     },
     duration: 1,
     opacity: 1,
     y: 0,
+    stagger: 0.5,
+  });
+
+  gsap.to('[data-element="fade-scale"]', {
+    scrollTrigger: {
+      trigger: '[data-element="fade-scale"]',
+      start: 'top 50%',
+    },
+    y: 1,
+    duration: 1,
+    scale: 1,
+    stagger: 0.5,
+    ease: 'back.out(1.7)',
+  });
+
+  gsap.to('[data-element="fade-in1"]', {
+    scrollTrigger: {
+      trigger: '[data-element="fade-in1"]',
+      start: 'bottom bottom',
+      end: 'top center',
+      scrub: true,
+    },
+    duration: 1,
+    opacity: 1,
+    y: 0,
+    stagger: 0.5,
+  });
+
+  gsap.to('[data-element="fade-scale1"]', {
+    scrollTrigger: {
+      trigger: '[data-element="fade-scale1"]',
+      start: 'bottom bottom',
+      end: 'top center',
+      scrub: true,
+    },
+    y: 1,
+    duration: 1,
+    scale: 1,
+    stagger: 0.5,
   });
 };
+
 
 export {animateBlocks};
